0454653baf12ff201d32b274a1fd8bde0c385c33
Seb Dalgarno <seb@poissonconsulting.ca>
2019-05-27 19:39:49 -0700

    add tests

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -29,3 +29,64 @@ test_that("check_table_name", {
                "table 'e' does not exist")
   expect_identical(check_table_name("local", conn), "local")
 })
+
+test_that("check_column_name", {
+  conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  teardown(DBI::dbDisconnect(conn))
+
+  local <- data.frame(test = 1:2)
+  expect_true(DBI::dbCreateTable(conn, "local", local))
+
+  expect_error(check_column_name("test", table_name = 1, exists = TRUE, conn),
+               "table_name must be class character")
+  expect_error(check_column_name("test", table_name = "e", exists = TRUE, conn),
+               "no such table: e")
+  expect_error(check_column_name(1, table_name = "local", exists = TRUE, conn),
+               "column_name must be class character")
+  expect_error(check_column_name("e", table_name = "local", exists = TRUE, conn),
+               "column 'e' does not exist")
+  expect_identical(check_column_name("test", table_name = "local", exists = TRUE, conn), "test")
+  expect_identical(check_column_name("e", table_name = "local", exists = FALSE, conn), "e")
+})
+
+test_that("check_column_blob", {
+  conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  teardown(DBI::dbDisconnect(conn))
+
+  local <- data.frame(test = 1:2)
+  expect_true(DBI::dbCreateTable(conn, "local", local))
+  expect_true(add_blob_column("blob", table_name = "local", conn = conn))
+
+  expect_error(check_column_blob("test", table_name = "local", conn),
+               "column 'test' is not type BLOB")
+  expect_error(check_column_blob("x", table_name = "local", conn),
+               "column 'x' does not exist")
+  expect_identical(check_column_blob("blob", table_name = "local", conn), "blob")
+})
+
+test_that("check_filter_key", {
+  conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  teardown(DBI::dbDisconnect(conn))
+
+  df <- data.frame(char = c("a", "b", "b"),
+                   num = c(1.1, 2.2, 2.2),
+                   key = c(1, 2, 3),
+                   stringsAsFactors = FALSE)
+  expect_true(DBI::dbWriteTable(conn, "df", df))
+  key <- df[1,]
+  key2 <- "a"
+  key3 <- data.frame(num = 1.1, key = 2)
+
+  expect_error(check_filter_key(table_name = "df", key = key2, conn),
+               "key must inherit from class data.frame")
+  expect_error(check_filter_key(table_name = "df", key = key3, conn),
+               "filtering table by key must result in a single observation")
+  expect_identical(check_filter_key(table_name = "df", key = key, conn), key)
+})
+
+test_that("check_flob_query", {
+ x <- list(NULL)
+ expect_error(check_flob_query(x), "there is no flob to retrieve")
+ x <- flobr::flob_obj
+ expect_identical(check_flob_query(x), x)
+})
