a6f0b31f336065a42818f5db911490d11a89b2d3
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-09 14:39:37 -0800

    fix appveyor build fail

--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -1,5 +1,5 @@
 test_that("ui functions work", {
-  expect_identical(ui_value("foo"), "\033[34m'foo'\033[39m")
+  expect_is(capture.output(ui_value("hi")), "character")
   expect_is(capture.output(ui_todo("hi")), "character")
   expect_is(capture.output(ui_done("hi")), "character")
   expect_is(capture.output(ui_line("hi")), "character")
