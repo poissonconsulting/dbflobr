21fd95689d1a7e137a8c7e21181461cc722f7a92
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-09 16:18:58 -0800

    move add_blob_column tests to read-write

--- tests/testthat/test-read-write.R
+++ tests/testthat/test-read-write.R
@@ -112,3 +112,32 @@ test_that("write_flob column exists", {
   expect_error(
     write_flob(flob, "New", "df", key, conn, exists = FALSE))
 })
+
+test_that("add_blob_column works", {
+  conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  teardown(DBI::dbDisconnect(conn))
+
+  df <- data.frame(x = 1)
+
+  expect_true(DBI::dbCreateTable(conn, "df", df))
+
+  ## add blob_column
+  expect_error(
+    add_blob_column(table_name = "df", column_name = "x", conn = conn))
+  expect_true(add_blob_column(table_name = "df", column_name = "flob", conn = conn))
+
+  df2 <- DBI::dbReadTable(conn, "df")
+  expect_identical(colnames(df2), c(colnames(df), c("flob")))
+  expect_is(df2$flob, "blob")
+
+  result <- DBI::dbSendQuery(conn = conn, statement = paste("SELECT flob FROM df LIMIT 1"))
+  expect_identical(DBI::dbColumnInfo(result)$type, "list")
+  DBI::dbClearResult(result)
+
+  ## flob
+  flob <- flobr::flob_obj
+  x <- collapse_flob(flob)
+  expect_is(x, "character")
+  expect_length(x, 1L)
+})
+
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,29 +1,11 @@
 context("utils")
 
-test_that("add_blob_column works", {
-  conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
-  teardown(DBI::dbDisconnect(conn))
+test_that("populate_key works", {
+  key2 <- data.frame(
+    char = "a", num = 2.2,
+    stringsAsFactors = FALSE
+  )
+  key3 <- data.frame(key = 3)
 
-  df <- data.frame(x = 1)
 
-  expect_true(DBI::dbCreateTable(conn, "df", df))
-
-  ## add blob_column
-  expect_error(
-    add_blob_column(table_name = "df", column_name = "x", conn = conn))
-  expect_true(add_blob_column(table_name = "df", column_name = "flob", conn = conn))
-
-  df2 <- DBI::dbReadTable(conn, "df")
-  expect_identical(colnames(df2), c(colnames(df), c("flob")))
-  expect_is(df2$flob, "blob")
-
-  result <- DBI::dbSendQuery(conn = conn, statement = paste("SELECT flob FROM df LIMIT 1"))
-  expect_identical(DBI::dbColumnInfo(result)$type, "list")
-  DBI::dbClearResult(result)
-
-  ## flob
-  flob <- flobr::flob_obj
-  x <- collapse_flob(flob)
-  expect_is(x, "character")
-  expect_length(x, 1L)
 })
