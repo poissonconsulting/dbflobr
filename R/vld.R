0f72b24559f760e27b0a61d9d6f7c04a0bdc5624
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-09-18 22:26:14 -0600

    added `vld_slob()` until it is on the CRAN version of flobr

--- R/vld.R
+++ R/vld.R
@@ -1,8 +1,11 @@
+# remove file once `flobr::vld_slob()` is on CRAN
 vld_exint <- function(x) {
   vld_s3_class(x, "exint") && vld_scalar(x) && vld_named(x) &&
     vld_s3_class(x[[1]], "integer") && vld_not_any_na(x[[1]])
 }
 
+vld_slob <- function(x) {
+  if (!(vld_s3_class(x, "blob") && vld_scalar(x) && vld_list(x))) {
     return(FALSE)
   }
 
@@ -12,5 +15,6 @@ vld_exint <- function(x) {
     return(FALSE)
   }
 
+  class(exint) <- "exint"
   vld_exint(exint)
 }
