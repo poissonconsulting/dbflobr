894b9079b00e2b9fbc8047f6bb629056a52f1b7e
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-09-18 22:25:47 -0600

    removed functions now on the CRAN version of `flobr`

--- R/vld.R
+++ R/vld.R
@@ -1,11 +1,8 @@
-# remove file once flobr 0.2.1 on CRAN
 vld_exint <- function(x) {
   vld_s3_class(x, "exint") && vld_scalar(x) && vld_named(x) &&
     vld_s3_class(x[[1]], "integer") && vld_not_any_na(x[[1]])
 }
 
-vld_flob <- function(x, old = FALSE) {
-  if (!(vld_s3_class(x, "flob") && vld_scalar(x))) {
     return(FALSE)
   }
 
@@ -15,6 +12,5 @@ vld_flob <- function(x, old = FALSE) {
     return(FALSE)
   }
 
-  if (old) class(exint) <- "exint"
   vld_exint(exint)
 }
